52c882b9cf23c1bf88c1c55fffbd19e07bc33fc0
ElLuigiii <luiisalvarez2018@gmail.com>
2022-04-16 07:45:49 -0300

    BaSP-M2022: Here have exercises to javascript

---
+++ Semana-5/Exercises/01_variables_and_operators.js
@@ -0,0 +1,15 @@
+var num1= 305
+var num2= 306
+var num3= num1+num2
+console.log('Respuesta del ejercicio n°1', num3)
+
+var salu= "Hello"
+var dando= "world"
+var saludando= salu + " " + dando
+console.log('Resultado del ejercicio n°2', saludando)
+
+var string1= "Practicando el ejercicio n°3"
+var string2= "sumando caracteres al string1"
+var string3= string1.length+string2.length
+
+console.log(string3)
