9fe7c51570f691e6ce74243f610a19fc7dc48ab5
ElLuigiii <luiisalvarez2018@gmail.com>
2022-04-17 14:50:38 -0300

    BaSP-M2022: Modified js and finished the excercises

--- Semana-5/Exercises/01_variables_and_operators.js
+++ Semana-5/Exercises/01_variables_and_operators.js
@@ -1,15 +1,22 @@
+//Exercises n°1
+//a)
 var num1= 305
 var num2= 306
 var num3= num1+num2
-console.log('Respuesta del ejercicio n°1', num3)
-
+console.log("Reply to exercises 1a) " + "'" +  num3 + "'")
+//b)
 var salu= "Hello"
 var dando= "world"
 var saludando= salu + " " + dando
-console.log('Resultado del ejercicio n°2', saludando)
-
+console.log("Reply to exercises 1b) " + "'" + saludando +"'")
+//c)
 var string1= "Practicando el ejercicio n°3"
 var string2= "sumando caracteres al string1"
 var string3= string1.length+string2.length
+console.log("Reply to exexrcises 1c) " + "'" + string3 + "'")
+//d)
+var nombre= "My name is Luis Álvarez"
+var myName= nombre.toUpperCase()
+console.log ("Reply to excercises 1d) " + "'" + myName + "'")
+/////////Exercises 1 finished  ////////////
 
-console.log(string3)
